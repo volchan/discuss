5c579ca3d857b583760f8208f15b0e185f0bc4b7
stephane de la torre <s.dlt@hotmail.fr>
2018-04-11 13:16:32 +0200

    fix typo for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :discuss, Discuss.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :discuss, ElixirTestApp.Repo,
+config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),

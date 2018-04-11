7f941eb7682d6bb3a39d87905e9a868ccc9be5f3
stephane de la torre <s.dlt@hotmail.fr>
2018-04-11 14:17:15 +0200

    removed port

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :discuss, Discuss.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "discuss-elixir.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "discuss-elixir.herokuapp.com"],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
